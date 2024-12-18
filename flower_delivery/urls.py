fdb39ffd59903b9ace69b78bb66c39ecedf22c84
Vadim <vadimkrupinin@gmail.com>
2024-12-18 21:09:47 +0000

    add VK registr

--- flower_delivery/urls.py
+++ flower_delivery/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from users.views import telegram_callback  # Импортируем нашу функцию для Telegram callback
 from . import views
 
 urlpatterns = [
@@ -19,6 +20,7 @@ urlpatterns = [
     path('reports/', include('reports.urls', namespace='reports')),
     path('api/link_telegram_id/', views.link_telegram_id, name='link_telegram_id'),
     path('oauth/', include('social_django.urls', namespace='social')),
+    path('oauth/complete/telegram-callback/', telegram_callback, name='telegram_callback'),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from rest_framework.decorators import api_view
 
 User = get_user_model()
 
@@ -32,6 +33,7 @@ class LinkTelegramIDAPIView(APIView):
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class GetTokenByTelegramIDAPIView(APIView):
     """
@@ -48,3 +50,30 @@ class GetTokenByTelegramIDAPIView(APIView):
             return Response({"token": token.key}, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({"error": f"Пользователь с telegram_id {telegram_id} не найден."}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@csrf_exempt
+def telegram_callback(request):
+    """
+    Обрабатывает POST-запрос от Telegram виджета.
+    Предполагается, что пользовательский объект будет в поле 'user' в теле запроса,
+    содержащий как минимум 'id' (Telegram user ID), и, возможно, 'username'.
+    """
+    user_data = request.data
+    tg_id = user_data.get('id')  # Telegram user ID
+    tg_username = user_data.get('username')
+
+    if not tg_id:
+        return Response({"error": "Telegram user id not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Создаём или получаем пользователя, связанного с данным Telegram ID
+    user, created = User.objects.get_or_create(
+        username=f'tg_{tg_id}',
+        defaults={'telegram_id': tg_id}
+    )
+
+    # Получаем или создаём токен для пользователя
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response({"message": "Telegram auth successful", "token": token.key}, status=status.HTTP_200_OK)
